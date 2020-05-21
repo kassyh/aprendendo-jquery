7701b2ead9a01b890376568e2a71c7cfc9b5b35e
kassyh <kassyanealvesc@gmail.com>
2020-05-21 16:32:19 -0300

    utilizando jQuery e Ajax; depois coloquei a class navbar do boostrap e a criação aplicando estilos em tabelas.

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+
+function consultarCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+$.ajax({
+    url: url,
+    type: "GET",
+    success: function(response){
+        console.log(response);
+        $("#titulo_cep").html("CEP  " + response.cep);
+        $("#logradouro").html(response.logradouro);
+        $("#bairro").html(response.bairro);
+        $("#localidade").html(response.localidade);
+        $("#uf").html(response.uf);
+    }
+})
+}
